3b53ea406073d3aa4abcec6d9441cda027546d4f
MohanCodes <mohanatkuri@gmail.com>
2024-11-10 22:25:01 -0600

    fix the weird line

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -8,7 +8,7 @@ const Footer: React.FC = () => {
   ];
 
   return (
-    <footer className="bg-cblack text-white py-8">
+    <footer className="bg-cblack text-white py-8 -mt-1">
       <div className="container mx-auto px-4">
         <div className="flex flex-col items-center">
           <div className="flex space-x-4 mb-4">
